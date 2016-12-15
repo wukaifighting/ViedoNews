719fe84901e8e859861ab78454ac140899000901
wukaifighting <826275981@qq.com>
2016-12-15 15:13:50 +0800

    12.15第一次上传

---
+++ videoplayer/src/main/java/com/viedonews/videoplayer/full/full/MyClass.java
@@ -0,0 +1,14 @@
+package com.viedonews.videoplayer.full.full;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2016/12/15.
+ */
+
+public class MyClass {
+    public void  show(Context context,String aaa){
+        Toast.makeText(context, "aaa", Toast.LENGTH_SHORT).show();
+    }
+}
